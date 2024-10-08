160abb81f42631cd35cb816e68a9e7c09978af70
Abijith-Anil <a00310598@mycambrian.ca>
2024-10-08 01:53:14 +0000

    Created ServiceClass

--- CalculatorApi/Program.cs
+++ CalculatorApi/Program.cs
@@ -6,6 +6,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddTransient<ICalculatorService<double>, CalculatorService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
